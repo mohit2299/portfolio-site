63f9e580476b62a5af540f87930ead9f32023072
Mohit Gupta <76578065+mohit2299@users.noreply.github.com>
2025-08-09 23:13:04 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Introduction from "@/components/Introduction";
-import Projects from "@/components/Projects";
-import Contact from "@/components/Contact";
+import Introduction from "../components/Introduction";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+
 
 export default function Page() {
   return (
