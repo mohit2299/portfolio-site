4685679b8a97b916ebde86c08a86c9f520d3dc42
Mohit Gupta <76578065+mohit2299@users.noreply.github.com>
2025-08-09 22:55:35 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Introduction from "@/components/Introduction";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen">
+      <Introduction />
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
